bf770b289c62042faf73cb3041faad97c1b3bbd2
Jean Luca Colombo <jeanlucacolombo@outlook.com>
2021-03-31 11:28:16 -0300

    Question 1 done.

---
+++ Question 1/Question1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+int printNum(int num)
+{
+    using namespace std;
+    string str;                 // local variable, destroyed after usage
+
+    if (num % 3 == 0)           // Checks it num is multiple of 3;
+        str.append("Foo");      // Appends "Foo" to str;
+
+    if (num % 5 == 0)           // Checks it num is multiple of 5;
+        str.append("Baa");      // Appends "Baa" to str;
+    
+    if (str.empty())
+        cout << num << endl;    // If str is empty, print num;
+    else    
+        cout << str << endl;    // Print str otherwise.
+
+    if (num >= 100)
+        return 0;
+    else
+        return printNum(++num);
+}
+
+int main()
+{
+    printNum(1);                // num is passed by value.
+}
